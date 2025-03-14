43bd3fe909efd5c37c25e77485abe1442e0e8c2e
Federico Paolinelli <fpaoline@redhat.com>
2025-03-14 05:06:43 -0400

    L2 demo with kubevirt + L3
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- demoocp/kubevirt.sh
+++ demoocp/kubevirt.sh
@@ -2,3 +2,17 @@
 
 kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/v1.4.0/kubevirt-operator.yaml
 kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/v1.4.0/kubevirt-cr.yaml
+kubectl apply -f https://github.com/nmstate/kubernetes-nmstate/releases/download/v0.83.0/nmstate.io_nmstates.yaml
+kubectl apply -f https://github.com/nmstate/kubernetes-nmstate/releases/download/v0.83.0/namespace.yaml
+kubectl apply -f https://github.com/nmstate/kubernetes-nmstate/releases/download/v0.83.0/service_account.yaml
+kubectl apply -f https://github.com/nmstate/kubernetes-nmstate/releases/download/v0.83.0/role.yaml
+kubectl apply -f https://github.com/nmstate/kubernetes-nmstate/releases/download/v0.83.0/role_binding.yaml
+kubectl apply -f https://github.com/nmstate/kubernetes-nmstate/releases/download/v0.83.0/operator.yaml
+
+sleep 4s
+cat <<EOF | kubectl create -f -
+apiVersion: nmstate.io/v1
+kind: NMState
+metadata:
+  name: nmstate
+EOF
